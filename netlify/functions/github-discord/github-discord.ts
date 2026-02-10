176a82cc6435261c5f290347344a22ba3388e92e
Moises Velasquez <moises007@gmail.com>
2026-02-09 19:40:30 -0500

    feat(webhook): integration webhook with github

--- netlify/functions/github-discord/github-discord.ts
+++ netlify/functions/github-discord/github-discord.ts
@@ -19,11 +19,55 @@ const notify = async (message: string) => {
   return true;
 };
 
+const onStar = (payload: any): string => {
+  let message: string = "";
+
+  const { action, sender, repository, starred_at } = payload;
+  message = `User ${sender.login} ${action} star on ${repository.full_name}`;
+
+  return message;
+};
+
+const onIssue = (payload: any): string => {
+  const { action, issue } = payload;
+
+  if (action === "opened") {
+    return `An issue was opened with this title ${issue.title}`;
+  }
+  if (action === "closed") {
+    return `An issue was closed with this title ${issue.user.login}`;
+  }
+  if (action === "reopened") {
+    return `An issue was reopened with this title ${issue.user.login}`;
+  }
+  return `Unknown action ${action}`;
+};
+
 const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext,
 ) => {
-  await notify("Hello world from Netlify Functions!");
+  const githubEvent = event.headers["x-github-event"] ?? "unknown";
+  const payload = JSON.parse(event.body ?? "{}");
+  let message: string = "Hello world from Netlify Functions!";
+
+  console.log(payload);
+
+  switch (githubEvent) {
+    case "star":
+      message = onStar(payload);
+      break;
+
+    case "issues":
+      message = onIssue(payload);
+      break;
+
+    default:
+      message = `Unknown event ${githubEvent}`;
+      break;
+  }
+
+  await notify(message);
 
   return {
     statusCode: 200,
